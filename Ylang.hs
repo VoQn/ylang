609bf6b5aa883074e53dc004ad7dc4829fbcce33
VoQn <voqn.tyrantist@gmail.com>
2014-08-25 03:49:56 +0900

    Added Simple Type system foundation

--- Ylang.hs
+++ Ylang.hs
@@ -1,6 +1,7 @@
 module Ylang where
 
 import Ylang.Syntax
+import Ylang.Type
 import Ylang.Lexer
 import Ylang.Parser
 import Ylang.Eval
---
+++ test/TypeSpec.hs
@@ -0,0 +1,23 @@
+module TypeSpec where
+
+import Test.Hspec
+
+import Ylang.Type
+
+spec :: Spec
+spec = do
+  describe "show ylang type" $ do
+
+    it "Bool type" $
+      show TyBool `shouldBe` "Bool"
+
+    it "Alpha type" $
+      (show $ TyVar 1) `shouldBe` "a1"
+
+    it "(Bool -> Bool) type" $
+      (show $ TyFunc TyBool TyBool)
+      `shouldBe` "Bool -> Bool"
+
+    it "Arrow (Bool -> Bool -> Bool)" $
+      (show $ TyFunc TyBool (TyFunc TyBool TyBool))
+      `shouldBe` "Bool -> Bool -> Bool"
