9beb3d4e5aedb49b849be717f17ed3b84a86093a
VoQn <voqn.tyrantist@gmail.com>
2014-08-30 01:56:36 +0900

    eval to be Monadic

--- Ylang/Eval.hs
+++ Ylang/Eval.hs
@@ -4,90 +4,74 @@ import Data.List (intercalate)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-import Ylang.Syntax hiding (name)
+import Control.Monad.Identity
 
-data Env = Env {
-    name   :: Name
-  , frames :: [Name]
-  , scope  :: Map Name Expr
-  }
+import Ylang.Syntax
 
-currentFrameName :: Env -> Name
-currentFrameName (Env n fs _) = case fs of
-  [] -> n
-  _  -> intercalate "." (n:(reverse fs))
+type Env = Map.Map Name Expr
+type Eval a = Identity a
+
+runEval :: Eval a -> a
+runEval = runIdentity
 
 defaultEnv :: Env
-defaultEnv = Env {
-      name   = "TopLevel"
-    , frames = []
-    , scope  = Map.empty
-  }
+defaultEnv = Map.empty
 
-assign :: Env -> Name -> Expr -> Env
-assign (Env n f s) sym expr =
-  let r = Map.insert sym expr s
-  in Env { name = n, frames = f, scope = r }
-
-eval :: Env -> Expr -> (Env, Expr)
+eval :: Monad m => Env -> Expr -> m Expr
 eval env expr = case expr of
   -- atomic
-  Boolean _ -> (env, expr)
-  Int     _ -> (env, expr)
-  Float   _ -> (env, expr)
-  Ratio   _ -> (env, expr)
-  String  _ -> (env, expr)
+  Boolean _ -> return expr
+  Int     _ -> return expr
+  Float   _ -> return expr
+  Ratio   _ -> return expr
+  String  _ -> return expr
 
   -- collection
-  Pair _ _ -> (env, expr)
-  Array  _ -> (env, expr)
+  Pair e1 e2 -> do
+    r1 <- eval env e1
+    r2 <- eval env e2
+    return $ Pair r1 r2
+
+  Array es ->
+    let
+      rs = do
+        e <- es
+        r <- eval env e
+        return $ r
+    in return $ Array rs
 
   -- factor
-  Define n v -> definition env n v
+  Define n v -> case Map.lookup n env of
+
+    -- already assigned
+    Just x ->
+      let
+        e1 = "<Conflict Definition> "
+        e2 = "Already Defined :: "
+        e3 = "But Reassigned :: "
+        ex = show $ Define n x
+        ev = show $ Define n v
+      in fail $ intercalate " " [e1,e2,ex,e3,ev]
+
+    -- can assign
+    Nothing -> do
+        v' <- eval env v
+        let env' = Map.insert n v' env
+        eval env' (Atom n)
 
   -- void
-  Factor [] -> (env, expr)
+  Factor []     -> return expr
 
   -- identity
-  Factor (e:[]) -> eval env e
+  Factor (e:[]) -> return e
 
   -- applycation
-  Factor (f:args) -> case f of
-    -- declare (type binding)
-    Atom ":"  -> declaration env args
+  Factor (f:args) -> undefined
 
-    -- create function (not evaluate this time)
-    Func a as es r -> anonymous env (a:as) es r args
-
-    -- other atomic something (maybe function)
-    g@(Atom _) ->
-      let (env', h) = apply env g args
-      in eval env' h
-
-    -- nested expression
-    Factor (g:args') ->
-      let (env', h) = apply env g args'
-      in eval env' (Factor (h:args))
-
-  Atom n -> case Map.lookup n (scope env) of
-    Just a  -> (env, a)
-
-    -- TODO undefineded atom case
-    Nothing -> undefined
-
-  _ -> (env, expr)
+  _ -> return expr
 
 declaration :: Env -> [Expr] -> (Env, Expr)
 declaration = undefined
-
-definition :: Env -> Name -> Expr -> (Env, Expr)
-definition e v body = case Map.lookup v (scope e) of
-  -- can assign
-  Nothing -> let e' = assign e v body in (e', Atom v)
-
-  -- cannot reassign
-  -- TODO should throw Error
-  Just _ -> undefined
 
 -- anonymous :: Env -> Expr -> [Expr] -> (Env, Expr)
 anonymous e vs es r as
@@ -99,14 +83,3 @@ anonymous e vs es r as
 
   | otherwise = -- arity over
       undefined
-
-apply :: Env -> Expr -> [Expr] -> (Env, Expr)
-apply e f [] = (e, f)
-
-apply e (Atom n) args = case Map.lookup n (scope e) of
-  Nothing -> undefined
-  Just g  ->
-    let (e', h) = eval e g
-    in eval e' (Factor (h:args))
-
-apply _ _ _ = undefined
--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -6,31 +6,40 @@ import Test.QuickCheck (property)
 import Ylang.Eval
 import Ylang.Syntax
 
+
 spec :: Spec
 spec = do
+
   describe "evaluate atomic value" $ do
     it "integer value (..., -2, -1, 0, 1, 2, ...)" $
-      property $ \ x ->
-        snd (eval defaultEnv $ Int x) `shouldBe` Int x
+      property $ \ x -> do
+        y <- eval defaultEnv (Int x)
+        y `shouldBe` Int x
 
     it "float value (..., 0.2, -0.1, 0.0, 0.1, 0.2, ...)" $
-      property $ \ x ->
-        snd (eval defaultEnv $ Float x) `shouldBe` Float x
+      property $ \ x -> do
+        y <- eval defaultEnv (Float x)
+        y `shouldBe` Float x
 
     it "rational value (..., -1/2, 1/1, -1/3, ...)" $
-      property $ \ x ->
-        snd (eval defaultEnv $ Ratio x) `shouldBe` Ratio x
+      property $ \ x -> do
+        y <- eval defaultEnv (Ratio x)
+        y `shouldBe` Ratio x
 
     it "boolean value (Yes / No)" $
-      property $ \ x ->
-        snd (eval defaultEnv $ Boolean x) `shouldBe` Boolean x
+      property $ \ x -> do
+        y <- eval defaultEnv (Boolean x)
+        y `shouldBe` Boolean x
 
   describe "evaluate sharrow definition assign" $ do
 
-    let fact = Define "x" (Int 10)
-    it "assign (= x 10)" $
-      snd (eval defaultEnv fact) `shouldBe` Atom "x"
-
-    let assignedEnv = fst $ eval defaultEnv fact
-    it "assigned value vall" $
-      snd (eval assignedEnv $ Atom "x") `shouldBe` Int 10
+    it "assign (= x 10)" $ do
+      let def = Define "x" (Int 10)
+      y <- eval defaultEnv def
+      y `shouldBe` Atom "x"
+{-
+    it "assigned value vall" $ do
+      let assignedEnv = eval defaultEnv fact
+      t <- eval assignedEnv (Atom "x")
+      y `shouldBe` Int 10
+-}
