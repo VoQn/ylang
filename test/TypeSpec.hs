1c8e7d5c246ab876f1380f6cc81ed6c303572e12
VoQn <voqn.tyrantist@gmail.com>
2014-08-30 15:45:40 +0900

    Added Builtin types

--- test/TypeSpec.hs
+++ test/TypeSpec.hs
@@ -7,20 +7,36 @@ import Ylang.Type
 spec :: Spec
 spec = do
   describe "show ylang type" $ do
+    it "Set type" $
+      show TySet `shouldBe` "Set"
 
-    it "Bool type" $
+    it "Boolean type" $
       show TyBool `shouldBe` "Bool"
 
-    it "Integer type" $
+    it "Integer number type" $
       show TyInt `shouldBe` "Int"
 
+    it "Rational number type" $
+      show TyRatio `shouldBe` "Ratio"
+
+    it "Floating point number type" $
+      show TyFloat `shouldBe` "Float"
+
+    it "Keyword type" $
+      show TyKeyword `shouldBe` "Keyword"
+
+    it "Charactor type" $
+      show TyChar `shouldBe` "Char"
+
+    it "String type" $
+      show TyString `shouldBe` "String"
+
     it "Alpha type" $
-      (show $ TyVar 1) `shouldBe` "a1"
+      (show $ TyVar "a" 1) `shouldBe` "a1"
 
-    it "(Bool -> Bool) type" $
-      (show $ TyFunc TyBool TyBool)
-      `shouldBe` "Bool -> Bool"
+    it "(-> Bool Bool) type" $
+      (show $ TyFunc TyBool TyBool) `shouldBe` "(-> Bool Bool)"
 
-    it "Arrow (Bool -> Bool -> Bool)" $
+    it "Arrow (-> Bool (-> Bool Bool))" $
       (show $ TyFunc TyBool (TyFunc TyBool TyBool))
-      `shouldBe` "Bool -> Bool -> Bool"
+      `shouldBe` "(-> Bool (-> Bool Bool))"
