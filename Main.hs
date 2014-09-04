4d5a7da452a2a24c914e0cd602e711625f68199b
VoQn <voqn.tyrantist@gmail.com>
2014-09-04 14:06:50 +0900

    Main.hs use LambdaCase

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import Control.Monad.Trans
@@ -13,36 +14,26 @@ import Ylang.Eval
 import Ylang.Display as D
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> repl
-    [fname] -> processFile fname >> return ()
+main = getArgs >>= \case
+  []      -> repl
+  [fname] -> processFile fname >> return ()
 
 repl :: IO ()
-repl = do
-  runInputT defaultSettings (loop initEnv)
+repl = runInputT defaultSettings $ loop initEnv
   where
-    loop env = do
-      minput <- getInputLine "ylsh> "
-      case minput of
-        Nothing    -> outputStrLn "Goodbye."
-        Just input -> do
-          env' <- liftIO $ process env input
-          case env' of
-            Just envn -> loop envn
-            Nothing   -> loop env
+  loop env = getInputLine "ylsh> " >>= \case
+    Nothing    -> outputStrLn "Goodbye."
+    Just input -> liftIO (process env input) >>= \case
+      Just newEnv -> loop newEnv
+      Nothing     -> loop env
 
 initEnv :: Env1
 initEnv = defaultEnv1
 
 process :: Env1 -> String -> IO (Maybe Env1)
-process env line = do
-  case parseTopLevel line of
-    Left  err  -> print err >> return Nothing
-    Right exprs -> do
-      envn <- evaluate env exprs
-      return $ Just envn
+process env line = case parseTopLevel line of
+  Left  err   -> print err          >>  return Nothing
+  Right exprs -> evaluate env exprs >>= return . Just
 
 processFile :: String -> IO (Maybe Env1)
 processFile fname = readFile fname >>= process initEnv
@@ -50,8 +41,7 @@ processFile fname = readFile fname >>= process initEnv
 evaluate :: Env1 -> [Expr] -> IO Env1
 evaluate env exs = do
   let exec = runEval1 env $ mapM eval1 exs
-  res <- getResult1 exec
-  case res of
+  getResult1 exec >>= \case
     Left  err -> liftIO $ print err
     Right val -> liftIO $ mapM_ D.display val
   getEnv1 exec
