53f0bdec24e45e85bfe3ee5cc9573a02d27e4d35
VoQn <voqn.tyrantist@gmail.com>
2014-10-04 02:09:04 +0900

    Complete IO Display test

--- test/Ylang/IOSpec.hs
+++ test/Ylang/IOSpec.hs
@@ -12,6 +12,18 @@ spec = describe "YLang IO Interface" $
 
   describe "Display type-class" $ do
 
+    it "can display IO" $
+      display ("hello" :: String) `shouldReturn` ()
+
+    describe "Lazy Text Builder is an instance of Display" $ do
+
+      it "buildText <empty> => \"\"" $
+        buildText ("" :: Builder) `shouldBe` ""
+
+      it "buildText \"something text\" => \"something text\"" $
+        buildText ("something text" :: Builder) `shouldBe`
+          "something text"
+
     describe "Char is an instance of Display" $
 
       it "ASCII charactor" $
