3025efc392009f731fb4a33e3571b8ce5d96f1ea
VoQn <voqn.tyrantist@gmail.com>
2014-10-04 17:06:55 +0900

    Refactoring IO test

--- test/Ylang/IOSpec.hs
+++ test/Ylang/IOSpec.hs
@@ -8,39 +8,39 @@ import Test.Hspec
 import Ylang.IO
 
 spec :: Spec
-spec = describe "YLang IO Interface" $
+spec = describe "YLang IO Interface" $ do
 
-  describe "Display type-class" $ do
+  describe "Display type-class" $
 
     it "can display IO" $
       display ("hello" :: String) `shouldReturn` ()
 
-    describe "Lazy Text Builder is an instance of Display" $ do
+  describe "Lazy Text Builder is an instance of Display" $ do
 
-      it "buildText <empty> => \"\"" $
-        buildText ("" :: Builder) `shouldBe` ""
+    it "buildText <empty> => \"\"" $
+      buildText ("" :: Builder) `shouldBe` ""
 
-      it "buildText \"something text\" => \"something text\"" $
-        buildText ("something text" :: Builder) `shouldBe`
-          "something text"
+    it "buildText \"something text\" => \"something text\"" $
+      buildText ("something text" :: Builder) `shouldBe`
+        "something text"
 
-    describe "Char is an instance of Display" $
+  describe "Char is an instance of Display" $
 
-      it "ASCII charactor" $
-        buildText 'c' `shouldBe` "c"
+    it "ASCII charactor" $
+      buildText 'c' `shouldBe` "c"
 
-    describe "text-build convinators" $ do
+  describe "text-build convinators" $ do
 
-      it "parens x -> \"(x)\"" $
-        parens "x" `shouldBe` "(x)"
+    it "parens x -> \"(x)\"" $
+      parens "x" `shouldBe` "(x)"
 
-      it "sep \":\" \"\" => \"\"" $
-        (sep ":" [] :: Builder) `shouldBe` ""
+    it "sep \":\" \"\" => \"\"" $
+      (sep ":" [] :: Builder) `shouldBe` ""
 
-      it "sep \",\" $ map buildText [1..5] => \"1,2,3,4,5\"" $
-        sep "," (map buildText ([1..5] :: [Int])) `shouldBe`
-          "1,2,3,4,5"
+    it "sep \",\" $ map buildText [1..5] => \"1,2,3,4,5\"" $
+      sep "," (map buildText ([1..5] :: [Int])) `shouldBe`
+        "1,2,3,4,5"
 
-      it "spaces $ map buildText [1..5] => \"1 2 3 4 5\"" $
-        spaces (map buildText ([1..5] :: [Int])) `shouldBe`
-          "1 2 3 4 5"
+    it "spaces $ map buildText [1..5] => \"1 2 3 4 5\"" $
+      spaces (map buildText ([1..5] :: [Int])) `shouldBe`
+        "1 2 3 4 5"
