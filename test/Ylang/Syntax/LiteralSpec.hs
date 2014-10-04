0abb0f5d838859098e3a05f231293ac2ce3fec70
VoQn <voqn.tyrantist@gmail.com>
2014-10-04 17:16:48 +0900

    Added Literal Test case. (char, string, keyword)

--- test/Ylang/Syntax/LiteralSpec.hs
+++ test/Ylang/Syntax/LiteralSpec.hs
@@ -9,7 +9,19 @@ import Ylang.Syntax.Literal
 spec :: Spec
 spec = describe "Ylang Literal" $
 
-  describe "as an instance of Display" $
+  describe "as an instance of Display" $ do
 
     it "buildText Yes => \"Yes\"" $
       buildText (LitBool True) `shouldBe` "Yes"
+
+    it "buildText No => \"No\"" $
+      buildText (LitBool False) `shouldBe` "No"
+
+    it "buildText 'c' => \"'c'\"" $
+      buildText (LitChr 'c') `shouldBe` "'c'"
+
+    it "buildText \"a text\" => \"\"a text\"\"" $
+      buildText (LitStr "a text") `shouldBe` "\"a text\""
+
+    it "buildText :keyword => \":keyword\"" $
+      buildText (LitKey "keyword") `shouldBe` ":keyword"
